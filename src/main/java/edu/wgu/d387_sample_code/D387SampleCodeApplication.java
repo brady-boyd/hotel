162cae126ea05ad21473dba8d75a99497631c249
brady-wgu <bboy102@wgu.edu>
2023-05-17 21:13:52 -0500

    Initial commit

---
+++ src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
@@ -0,0 +1,15 @@
+package edu.wgu.d387_sample_code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class D387SampleCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D387SampleCodeApplication.class, args);
+	}
+
+}
